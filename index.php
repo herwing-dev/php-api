1053fed70e27988fd647e3e8f0f02ee85f717ff0
Eduardo Macias <herwingmacias@gmail.com>
2024-10-31 23:53:12 -0600

    Add bootstrap

--- index.php
+++ index.php
@@ -23,31 +23,29 @@ curl_close($ch);
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>PHP API</title>
-  <link
-    rel="stylesheet"
-    href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.classless.min.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
-<body>
-  <main>
-    <section>
-      <img src="<?= $data["poster_url"]; ?>" alt="Poster de <?= $data["title"] ?>" width="250">
-    </section>
-    <hgroup>
-      <h3><?= $data["title"] ?> se estrena en <?= $data["days_until"]; ?> días</h3>
-      <p>Fecha de estreno: <?= $data["release_date"]; ?> </p>
-      <p>La siguiente es: <?= $data["following_production"]["title"]; ?> </p>
-    </hgroup>
+<body data-bs-theme="dark">
+  <main class="container d-flex justify-content-center align-items-center" style="height: 100vh;>
+    <div class="col-4">
+
+      <section class="card">
+        <img class="img-fluid mx-auto d-block" src="<?= $data["poster_url"]; ?>" alt="Poster de <?= $data["title"] ?>" width="200">
+        <hgroup class="card-body text-center">
+          <h3 class="card-title fs-4"><?= $data["title"] ?> se estrena en <?= $data["days_until"]; ?> días</h3>
+          <p class="card-text fs-6">Fecha de estreno: <?= $data["release_date"]; ?> </p>
+          <p class="card-text fs-6">La siguiente es: <?= $data["following_production"]["title"]; ?> </p>
+        </hgroup>
+      </section>
+    </div>
   </main>
   <style>
-    section {
-      text-align: center;
-    }
-
-    hgroup {
-      text-align: center;
+    :root {
+      color-scheme: dark;
     }
   </style>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
